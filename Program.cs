89efc48bfa4c75f1b77a68a5e92c0bbbaa11bd20
x54x6fx6d <tom31schule@web.de>
2023-07-15 00:30:10 +0200

    Print Command

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+namespace baelle;
+
+public class Baelle {
+    public static void Main(string[] args){
+        string path = args[0];
+        if (!(path.EndsWith(".baelle"))){
+            Console.WriteLine("Invalid File Type!!!!!!! Bälle hehehe");
+        } else {
+            string[] lines = File.ReadAllLines(path);
+            string string_pattern = "\"([^\"]*)\"";
+            foreach (var line in lines){
+                if (line.StartsWith("ball")){
+                    string content = "";
+                    MatchCollection matches = Regex.Matches(line, string_pattern);
+                    foreach(Match match in matches){
+                        string captured = match.Groups[1].Value;
+                        content += captured;
+                    }
+                    Console.WriteLine(content);
+                }
+            }
+        }
+        
+    }
+}
