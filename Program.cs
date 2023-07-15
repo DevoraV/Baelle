741c2cdb841ff247b9e86d3f3f9a7c532e7ef8e0
x54x6fx6dx6dx79 <tom31schule@web.de>
2023-07-15 14:35:25 +0200

    initiial var handler

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ namespace baelle;
 public class Baelle {
     public static void Main(string[] args){
         string path = args[0];
+        IDictionary<string, string> constString = new Dictionary<string, string>();
         if (!(path.EndsWith(".baelle"))){
             Console.WriteLine("Invalid File Type!!!!!!! Bälle hehehe");
         } else {
             string[] lines = File.ReadAllLines(path);
             string string_pattern = "\"([^\"]*)\"";
             foreach (var line in lines){
+
+                // Print Command
                 if (line.StartsWith("ball")){
                     string content = "";
                     MatchCollection matches = Regex.Matches(line, string_pattern);
@@ -18,8 +21,25 @@ public class Baelle {
                         string captured = match.Groups[1].Value;
                         content += captured;
                     }
+
                     Console.WriteLine(content);
                 }
+                // Konstante Variablen
+                else if (line.StartsWith("aufgeblasen")){
+                    string[] zeile = line.Split(" ");
+                    if (zeile[1] == "tennis"){
+                        Console.WriteLine(zeile[2] + " = " + zeile[3]);
+                    }
+
+                }
+                // Variablen
+                else if (line.StartsWith("platt")){
+                    string[] zeile = line.Split(" ");
+                    if (zeile[1] == "tennis"){
+
+                    }
+                }
+
             }
         }
         
--- Readme.md
+++ Readme.md
@@ -1,1 +1,13 @@
 # Bälle Interpreter
+
+
+## Variablen
+aufgeblasen = const
+platt != const
+
+tennis = string
+fussball = int
+wasserball = float
+billiard = double
+[ tennis ] = tennis[]
+{ key: wasserball } = { key: float }
